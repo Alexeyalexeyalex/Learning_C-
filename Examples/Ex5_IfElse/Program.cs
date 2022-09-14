9eae9770476353b35d29bf6c12d4280554bd9eaf
Alexey Silaenkov <silaenckov2014@yandex.ru>
2022-09-14 14:07:18 +0300

    Example 5 IfElse

---
+++ Examples/Ex5_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ураааа ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
